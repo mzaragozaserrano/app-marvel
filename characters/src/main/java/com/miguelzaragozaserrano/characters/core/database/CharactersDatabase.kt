4077f42d22ac305f95ce2f90ac5d1be13b4de47f
mzaragozaserrano <mzaragoza@pecuniacards.es>
2022-03-28 10:55:41 +0200

    characters | added | database

---
+++ characters/src/main/java/com/miguelzaragozaserrano/characters/core/database/CharactersDatabase.kt
@@ -0,0 +1,15 @@
+package com.miguelzaragozaserrano.characters.core.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.miguelzaragozaserrano.characters.core.dao.CharactersDAO
+import com.miguelzaragozaserrano.characters.data.models.entity.CharactersEntity
+import com.miguelzaragozaserrano.characters.data.utils.CharacterConverter
+
+@Database(entities = [CharactersEntity::class], version = 1)
+@TypeConverters(CharacterConverter::class)
+abstract class CharactersDatabase : RoomDatabase() {
+
+    abstract fun characterDao(): CharactersDAO
+}
