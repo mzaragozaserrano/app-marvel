36ee3f2fef9b0dd1a7f53870aeb4ab2b094b33be
mzaragozaserrano <mzaragoza@pecuniacards.es>
2022-03-28 10:51:29 +0200

    characters | added | endpoints

---
+++ characters/src/main/java/com/miguelzaragozaserrano/characters/data/service/CharacterApi.kt
@@ -0,0 +1,26 @@
+package com.miguelzaragozaserrano.characters.data.service
+
+import com.miguelzaragozaserrano.characters.data.models.entity.CharacterEntity
+import com.miguelzaragozaserrano.characters.data.models.response.Characters
+import com.miguelzaragozaserrano.core.base.BaseResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+internal interface CharacterApi {
+
+    companion object {
+        const val CHARACTERS = "/v1/public/characters"
+        const val CHARACTER = "/v1/public/characters/{characterId}"
+    }
+
+    @GET(CHARACTERS)
+    suspend fun getCharacters(
+        @Query("limit") limit: Int? = 10,
+        @Query("offset") offset: Int? = 0,
+    ): Response<BaseResponse<Characters>>
+
+    @GET(CHARACTER)
+    suspend fun getCharacter(@Path("characterId") id: String?): Response<BaseResponse<CharacterEntity>>
+}
