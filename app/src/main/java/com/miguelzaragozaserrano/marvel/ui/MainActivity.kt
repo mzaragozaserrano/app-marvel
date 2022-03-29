6ec68618714f7b9e44c22a2efa6c95b2947627ea
PECUNIADS\mzaragoza <mzaragoza@pecuniacards.es>
2022-03-29 16:10:16 +0200

    master | added | @AndroidEntryPoint activity

--- app/src/main/java/com/miguelzaragozaserrano/marvel/ui/MainActivity.kt
+++ app/src/main/java/com/miguelzaragozaserrano/marvel/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.miguelzaragozaserrano.marvel
+package com.miguelzaragozaserrano.marvel.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.miguelzaragozaserrano.marvel.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
