709baa00cef503b17e914aec48bd2fb60889247f
mzaragozaserrano <mzaragoza@pecuniacards.es>
2022-03-30 00:48:45 +0200

    master | edited | custom error

---
+++ data/src/main/java/com/miguelzaragozaserrano/characters/utils/Error.kt
@@ -0,0 +1,16 @@
+package com.miguelzaragozaserrano.characters.utils
+
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed class Error {
+    object Connectivity : Error()
+    class Server(val code: Int) : Error()
+    class Unknown(val message: String?) : Error()
+}
+
+fun Exception.toError(): Error = when (this) {
+    is IOException -> Error.Connectivity
+    is HttpException -> Error.Server(code())
+    else -> Error.Unknown(message ?: "")
+}
