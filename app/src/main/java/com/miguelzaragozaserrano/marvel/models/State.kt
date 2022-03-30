7823080f1dcbd0b75f52d7706414e82416ee1d58
PECUNIADS\mzaragoza <mzaragoza@pecuniacards.es>
2022-03-30 13:55:54 +0200

    master | added | generic state

--- app/src/main/java/com/miguelzaragozaserrano/marvel/models/State.kt
+++ app/src/main/java/com/miguelzaragozaserrano/marvel/models/State.kt
@@ -3,8 +3,8 @@ package com.miguelzaragozaserrano.marvel.models
 import com.miguelzaragozaserrano.data.utils.Error
 import com.miguelzaragozaserrano.marvel.utils.Status
 
-data class CharactersState(
+data class State<T>(
     var status: Status = Status.LOADING,
     var error: Error = Error.Unknown(""),
-    var success: CharactersView = CharactersView.empty(),
+    var success: T? = null,
 )
