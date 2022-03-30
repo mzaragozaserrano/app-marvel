848127ec63c166443adf42e87526aace5ce1daa3
PECUNIADS\mzaragoza <mzaragoza@pecuniacards.es>
2022-03-30 13:56:23 +0200

    master | added | state collect extension

---
+++ app/src/main/java/com/miguelzaragozaserrano/marvel/utils/extensions/Lifecycle.kt
@@ -0,0 +1,33 @@
+package com.miguelzaragozaserrano.marvel.utils.extensions
+
+import androidx.lifecycle.LifecycleOwner
+import com.miguelzaragozaserrano.data.utils.Error
+import com.miguelzaragozaserrano.marvel.models.State
+import com.miguelzaragozaserrano.marvel.utils.Status
+import kotlinx.coroutines.flow.StateFlow
+
+suspend fun <T : Any, L : StateFlow<T>> LifecycleOwner.collect(
+    flow: L,
+    loading: () -> Unit,
+    loaded: (Any?) -> Unit,
+    error: (String?) -> Unit
+) {
+    flow.collect {
+        val state = (it as State<*>)
+        when (state.status) {
+            Status.LOADING -> {
+                loading.invoke()
+            }
+            Status.LOADED -> {
+                loaded.invoke(state.success)
+            }
+            Status.ERROR -> {
+                when (state.error) {
+                    Error.Connectivity -> error.invoke("Error en la conexión")
+                    is Error.Server -> error.invoke("Error en el servidor")
+                    is Error.Unknown -> error.invoke((state.error as Error.Unknown).message)
+                }
+            }
+        }
+    }
+}
