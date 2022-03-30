60297d300a55de9ee3e593d0bd96e39b5be7af05
PECUNIADS\mzaragoza <mzaragoza@pecuniacards.es>
2022-03-30 13:54:13 +0200

    master | edited | refactoring function's name

--- app/src/main/java/com/miguelzaragozaserrano/marvel/base/BaseActivity.kt
+++ app/src/main/java/com/miguelzaragozaserrano/marvel/base/BaseActivity.kt
@@ -1,8 +1,14 @@
 package com.miguelzaragozaserrano.marvel.base
 
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.viewbinding.ViewBinding
+import com.airbnb.lottie.LottieAnimationView
+import com.google.android.material.snackbar.Snackbar
+import com.miguelzaragozaserrano.marvel.R
 
 abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
 
@@ -31,19 +37,50 @@ abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
     abstract fun getViewBinding(): VB
 
     private fun setupInit() {
-        setup1Toolbar()
-        setup2Menu()
-        setup3Navigation()
-        setup4Vars()
-        setup5InitFunctions()
-        setup6ClickListeners()
+        setupToolbar()
+        setupMenu()
+        setupNavigation()
+        setupVars()
+        setupInitFunctions()
+        setupClickListeners()
     }
 
-    open fun setup1Toolbar() {}
-    open fun setup2Menu() {}
-    open fun setup3Navigation() {}
-    open fun setup4Vars() {}
-    open fun setup5InitFunctions() {}
-    open fun setup6ClickListeners() {}
+    open fun setupToolbar() {}
+    open fun setupMenu() {}
+    open fun setupNavigation() {}
+    open fun setupVars() {}
+    open fun setupInitFunctions() {}
+    open fun setupClickListeners() {}
+
+    fun showProgressDialog() {
+        if (!progressDialog.isShowing) {
+            animation.playAnimation()
+            progressDialog.show()
+        }
+    }
+
+    fun hideProgressDialog() {
+        if (progressDialog.isShowing) {
+            animation.cancelAnimation()
+            progressDialog.dismiss()
+        }
+    }
+
+    fun snackBarLong(
+        view: View,
+        text: String,
+        colorBackground: Int,
+        colorText: Int
+    ) {
+        val snackBar = Snackbar.make(view, text, Snackbar.LENGTH_LONG)
+        snackBar.view.setBackgroundColor(
+            ContextCompat.getColor(
+                applicationContext,
+                colorBackground
+            )
+        )
+        snackBar.setTextColor(ContextCompat.getColor(applicationContext, colorText))
+        snackBar.show()
+    }
 
 }
