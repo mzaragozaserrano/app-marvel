b71382b4694989658672e0ebd94fccca0a3e595d
PECUNIADS\mzaragoza <mzaragoza@pecuniacards.es>
2022-03-31 13:53:00 +0200

    master | added | function for update mutablestateflow in viewmodels

--- app/src/main/java/com/miguelzaragozaserrano/marvel/utils/extensions/Lifecycle.kt
+++ app/src/main/java/com/miguelzaragozaserrano/marvel/utils/extensions/Lifecycle.kt
@@ -3,7 +3,7 @@ package com.miguelzaragozaserrano.marvel.utils.extensions
 import com.miguelzaragozaserrano.data.utils.*
 import com.miguelzaragozaserrano.marvel.models.UiState
 import com.miguelzaragozaserrano.marvel.utils.Status
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.*
 
 suspend fun <T : Any, L : StateFlow<T>> collect(
     flow: L,
@@ -30,4 +30,33 @@ suspend fun <T : Any, L : StateFlow<T>> collect(
             }
         }
     }
+}
+
+suspend fun <T : Any, G: Any> Flow<Result<T>>.update(
+    _state: MutableStateFlow<UiState<T, G>>,
+    onSuccess: (Success<T>) -> Unit
+) {
+    onStart { _state.update { it.copy(status = Status.LOADING) } }
+        .onCompletion { _state.update { it.copy(status = Status.LOADED) } }
+        .catch { throwable ->
+            _state.update {
+                UiState(
+                    status = Status.ERROR,
+                    error = Error.Unknown(throwable.message)
+                )
+            }
+        }
+        .collect { either ->
+            either.onFailure { failure ->
+                _state.update {
+                    it.copy(
+                        status = Status.ERROR,
+                        error = failure.error
+                    )
+                }
+            }
+            either.onSuccess { success ->
+                onSuccess.invoke(success)
+            }
+        }
 }
