c2b5e92c3ff08ef727bf0867ee7ef07bd333c25a
PECUNIADS\mzaragoza <mzaragoza@pecuniacards.es>
2022-04-01 12:17:56 +0200

    master | added | new extension or not info

--- app/src/main/java/com/miguelzaragozaserrano/marvel/utils/extensions/Converter.kt
+++ app/src/main/java/com/miguelzaragozaserrano/marvel/utils/extensions/Converter.kt
@@ -3,13 +3,16 @@ package com.miguelzaragozaserrano.marvel.utils.extensions
 import com.miguelzaragozaserrano.data.models.response.Character
 import com.miguelzaragozaserrano.data.models.response.Characters
 import com.miguelzaragozaserrano.data.utils.extensions.orEmpty
+import com.miguelzaragozaserrano.data.utils.extensions.orNotInfo
 import com.miguelzaragozaserrano.marvel.models.CharacterView
 import com.miguelzaragozaserrano.marvel.models.CharactersView
 
+
+
 fun Character.toCharacterView(): CharacterView = CharacterView(
     id = id.orEmpty(),
-    name = name.orEmpty(),
-    description = description.orEmpty(),
+    name = name.orNotInfo(),
+    description = description.orNotInfo(),
     modified = modified,
     resourceURI = resourceURI.orEmpty(),
     favorite = null,
--- data/src/main/java/com/miguelzaragozaserrano/data/utils/extensions/String.kt
+++ data/src/main/java/com/miguelzaragozaserrano/data/utils/extensions/String.kt
@@ -5,6 +5,14 @@ import java.security.MessageDigest
 
 fun String.Companion.empty() = ""
 
+fun String?.orNotInfo(): String {
+    return if (this.isNullOrEmpty()) {
+        "Information not available."
+    } else {
+        this
+    }
+}
+
 fun String.md5(): String {
     val md = MessageDigest.getInstance("MD5")
     return BigInteger(1, md.digest(toByteArray())).toString(16).padStart(32, '0')
