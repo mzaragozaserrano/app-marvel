7b6411b274e358d1789b3650dd35cd97b22f5790
mzaragozaserrano <mzaragoza@pecuniacards.es>
2022-07-30 12:14:06 +0200

    master | edited | now room returns characters sorted by name

--- data/src/main/java/com/miguelzaragozaserrano/data/dao/CharactersDAO.kt
+++ data/src/main/java/com/miguelzaragozaserrano/data/dao/CharactersDAO.kt
@@ -11,10 +11,10 @@ interface CharactersDAO : BaseDao<CharacterEntity> {
     @Query("SELECT COUNT(id) FROM CharacterEntity")
     fun characterCount(): Int
 
-    @Query("SELECT * FROM CharacterEntity")
+    @Query("SELECT * FROM CharacterEntity ORDER BY name ASC")
     fun getAll(): List<CharacterEntity>
 
-    @Query("SELECT * FROM CharacterEntity LIMIT :limit OFFSET :offset")
+    @Query("SELECT * FROM CharacterEntity ORDER BY name ASC LIMIT :limit OFFSET :offset")
     fun getSome(limit: Int = 10, offset: Int): List<CharacterEntity>
 
 }
