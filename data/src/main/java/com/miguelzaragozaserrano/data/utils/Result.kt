59b88631574634b61aaae09d47f82b75911cc46b
PECUNIADS\mzaragoza <mzaragoza@pecuniacards.es>
2022-08-11 13:58:00 +0200

    master | edited | now errors show a code

--- data/src/main/java/com/miguelzaragozaserrano/data/utils/Result.kt
+++ data/src/main/java/com/miguelzaragozaserrano/data/utils/Result.kt
@@ -1,5 +1,7 @@
 package com.miguelzaragozaserrano.data.utils
 
+import com.miguelzaragozaserrano.data.utils.CodeError.*
+
 typealias Result<T> = Either<Failure, Success<T>>
 
 sealed class State<out T : Any>
@@ -8,8 +10,13 @@ class Success<out T : Any>(val data: T) : State<T>()
 class Failure(val error: Error) : State<Nothing>()
 
 sealed class Error {
-    class Server(val code: Int) : Error()
-    class Unknown(val message: String?) : Error()
-    data class Connectivity (val message: String? = "Error en el servidor") : Error()
+    class NoResults(val code: CodeError = NO_RESULTS) : Error()
+    class Server(val code: CodeError = SERVER_ERROR) : Error()
+    class Unknown(val code: CodeError = GENERIC_ERROR) : Error()
+    data class Connectivity(val code: CodeError = NO_CONNECTIVITY) : Error()
     data class Throwable(val throwable: kotlin.Throwable?) : Error()
+}
+
+enum class CodeError {
+    NO_RESULTS, NO_CONNECTIVITY, GENERIC_ERROR, SERVER_ERROR
 }
