d423992fc6b9c6d371fcc117c06af8fc2ff9f853
PECUNIADS\mzaragoza <mzaragoza@pecuniacards.es>
2022-08-11 13:59:25 +0200

    master | added | new execute for searching characters by name

--- app/src/main/java/com/miguelzaragozaserrano/marvel/characters/CharactersViewModel.kt
+++ app/src/main/java/com/miguelzaragozaserrano/marvel/characters/CharactersViewModel.kt
@@ -1,14 +1,16 @@
 package com.miguelzaragozaserrano.marvel.characters
 
-import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.miguelzaragozaserrano.data.models.response.Characters
-import com.miguelzaragozaserrano.domain.usecases.GetCharacters
+import com.miguelzaragozaserrano.domain.usecases.CharactersByNameUseCaseImpl
 import com.miguelzaragozaserrano.domain.usecases.CharactersUseCaseImpl
+import com.miguelzaragozaserrano.domain.usecases.FindCharactersByName
+import com.miguelzaragozaserrano.domain.usecases.GetCharacters
 import com.miguelzaragozaserrano.marvel.base.BaseViewModel
+import com.miguelzaragozaserrano.marvel.models.CharacterView
 import com.miguelzaragozaserrano.marvel.models.CharactersView
 import com.miguelzaragozaserrano.marvel.models.UiState
-import com.miguelzaragozaserrano.marvel.utils.Status
+import com.miguelzaragozaserrano.marvel.utils.Status.LOADED
 import com.miguelzaragozaserrano.marvel.utils.extensions.cancelIfActive
 import com.miguelzaragozaserrano.marvel.utils.extensions.toCharactersView
 import com.miguelzaragozaserrano.marvel.utils.extensions.update
@@ -21,14 +23,26 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class CharactersViewModel @Inject constructor(private val getCharacters: @JvmSuppressWildcards GetCharacters) :
-    BaseViewModel() {
+class CharactersViewModel @Inject constructor(
+    private val getCharacters: @JvmSuppressWildcards GetCharacters,
+    private val findCharactersByName: @JvmSuppressWildcards FindCharactersByName
+) : BaseViewModel() {
 
-    var offset = 0
+    private var offset = 0
     private var getCharactersJob: Job? = null
+    private val listCharacters: MutableList<CharacterView> = mutableListOf()
 
     private var _charactersState = MutableStateFlow(UiState<Characters, CharactersView>())
     val charactersState get() = _charactersState.asStateFlow()
+
+    private var _findCharactersState = MutableStateFlow(UiState<Characters, CharactersView>())
+    val findCharactersState get() = _findCharactersState.asStateFlow()
+
+    fun addCharacters(list: MutableList<CharacterView>) {
+        listCharacters.addAll(list)
+    }
+
+    fun getListCharacters() = listCharacters
 
     fun executeGetCharacters(fromPagination: Boolean = false) {
         getCharactersJob.cancelIfActive()
@@ -40,7 +54,22 @@ class CharactersViewModel @Inject constructor(private val getCharacters: @JvmSup
                             offset += newOffset
                         }
                         it.copy(
-                            status = Status.LOADED,
+                            status = LOADED,
+                            success = state.data.toCharactersView()
+                        )
+                    }
+                }
+        }
+    }
+
+    fun executeFindCharactersByName(query: String = "") {
+        getCharactersJob.cancelIfActive()
+        getCharactersJob = viewModelScope.launch {
+            findCharactersByName(CharactersByNameUseCaseImpl.Params(query))
+                .update(_findCharactersState) { state ->
+                    _findCharactersState.update {
+                        it.copy(
+                            status = LOADED,
                             success = state.data.toCharactersView()
                         )
                     }
